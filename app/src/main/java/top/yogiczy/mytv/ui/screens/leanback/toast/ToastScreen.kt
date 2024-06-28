ab8767f56a7de6827f34bcdf27568c0c40dbd8dc
yaoxieyoulei <1622968661@qq.com>
2024-06-28 14:39:39 +0800

    :bug: 修复手机端无法点击

--- app/src/main/java/top/yogiczy/mytv/ui/screens/leanback/toast/ToastScreen.kt
+++ app/src/main/java/top/yogiczy/mytv/ui/screens/leanback/toast/ToastScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
@@ -22,7 +21,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
 import androidx.tv.material3.Icon
@@ -35,20 +36,17 @@ fun LeanbackToastScreen(
     modifier: Modifier = Modifier,
     state: LeanbackToastState = rememberLeanbackToastState(),
 ) {
-    Box(modifier = modifier.fillMaxSize()) {
-        Popup {
-            Box(modifier = Modifier.fillMaxSize()) {
-                AnimatedVisibility(
-                    visible = state.visible,
-                    enter = fadeIn() + scaleIn(),
-                    exit = fadeOut() + scaleOut(),
-                    modifier = Modifier
-                        .align(Alignment.BottomCenter)
-                        .padding(bottom = 28.dp),
-                ) {
-                    LeanbackToastItem(property = state.current)
-                }
-            }
+    Popup(
+        alignment = Alignment.BottomCenter,
+        offset = IntOffset(0, with(LocalDensity.current) { -28.dp.toPx().toInt() }),
+    ) {
+        AnimatedVisibility(
+            visible = state.visible,
+            enter = fadeIn() + scaleIn(),
+            exit = fadeOut() + scaleOut(),
+            modifier = modifier,
+        ) {
+            LeanbackToastItem(property = state.current)
         }
     }
 }
