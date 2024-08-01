a861e095b436f3a0568d6beecf46c3c670a8ccc5
yaoxieyoulei <1622968661@qq.com>
2024-08-01 09:14:25 +0800

    :bookmark: v1.4.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         minSdk = 21
         targetSdk = 34
         versionCode = 1
-        versionName = "1.4.2"
+        versionName = "1.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
