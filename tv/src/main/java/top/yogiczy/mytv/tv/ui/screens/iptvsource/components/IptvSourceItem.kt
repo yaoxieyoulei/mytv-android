25217eafa3e87ce8e494aa7894cbf130edb79fc5
yaoxieyoulei <1622968661@qq.com>
2024-08-14 09:07:04 +0800

    :bug: 移除自动获取直播源信息，可能导致部分低内存设备OOM

--- tv/src/main/java/top/yogiczy/mytv/tv/ui/screens/iptvsource/components/IptvSourceItem.kt
+++ tv/src/main/java/top/yogiczy/mytv/tv/ui/screens/iptvsource/components/IptvSourceItem.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -20,12 +19,9 @@ import androidx.compose.ui.unit.dp
 import androidx.tv.material3.ListItem
 import androidx.tv.material3.RadioButton
 import androidx.tv.material3.Text
-import top.yogiczy.mytv.core.data.entities.channel.ChannelGroupList
 import top.yogiczy.mytv.core.data.entities.iptvsource.IptvSource
-import top.yogiczy.mytv.core.data.repositories.iptv.IptvRepository
 import top.yogiczy.mytv.tv.ui.material.Tag
 import top.yogiczy.mytv.tv.ui.theme.MyTVTheme
-import top.yogiczy.mytv.tv.ui.utils.Configs
 import top.yogiczy.mytv.tv.ui.utils.focusOnLaunchedSaveable
 import top.yogiczy.mytv.tv.ui.utils.handleKeyEvents
 import top.yogiczy.mytv.tv.ui.utils.ifElse
@@ -43,18 +39,6 @@ fun IptvSourceItem(
 
     val focusRequester = remember { FocusRequester() }
     var isFocused by remember { mutableStateOf(false) }
-
-    var channelGroupList by remember { mutableStateOf(ChannelGroupList()) }
-
-    LaunchedEffect(Unit) {
-        if (channelGroupList.isEmpty()) {
-            channelGroupList = try {
-                IptvRepository(iptvSource).getChannelGroupList(cacheTime = Configs.iptvSourceCacheTime)
-            } catch (ex: Exception) {
-                ChannelGroupList()
-            }
-        }
-    }
 
     ListItem(
         modifier = modifier
@@ -84,14 +68,6 @@ fun IptvSourceItem(
                 iptvSource.url,
                 maxLines = if (isFocused) Int.MAX_VALUE else 1,
             )
-
-            if (channelGroupList.isNotEmpty()) {
-                val totalChannelGroupCount = channelGroupList.size
-                val totalChannelCount = channelGroupList.sumOf { it.channelList.size }
-                Text("共${totalChannelGroupCount}个分组，${totalChannelCount}个频道")
-            } else {
-                Text("")
-            }
         },
         trailingContent = {
             RadioButton(selected = isSelected, onClick = onSelected)
