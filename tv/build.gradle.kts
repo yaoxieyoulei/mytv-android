971398fc0caab55305434bc5614edbaa03bb475e
yaoxieyoulei <1622968661@qq.com>
2024-08-16 12:45:15 +0800

    :art: 跟换media3 ffmpeg aar

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -43,6 +43,7 @@ android {
     }
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
@@ -99,4 +100,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    coreLibraryDesugaring(libs.desugar.jdk.libs)
 }
