adccb982679f567d8d7727fd56f67841911b0329
yaoxieyoulei <1622968661@qq.com>
2024-08-17 13:36:58 +0800

    :sparkles: tv2.2.0

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 2
-        versionName = "2.1.2"
+        versionName = "2.2.0"
         vectorDrawables {
             useSupportLibrary = true
         }
