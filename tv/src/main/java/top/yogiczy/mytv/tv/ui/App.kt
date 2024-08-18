8c1f99de691f4c37040b51787be24047118b0616
yaoxieyoulei <1622968661@qq.com>
2024-08-18 20:27:25 +0800

    :art: 优化权限

--- tv/src/main/java/top/yogiczy/mytv/tv/ui/App.kt
+++ tv/src/main/java/top/yogiczy/mytv/tv/ui/App.kt
@@ -1,5 +1,14 @@
 package top.yogiczy.mytv.tv.ui
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.Settings
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.IntRange
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
@@ -13,16 +22,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.consumeAsFlow
 import kotlinx.coroutines.flow.debounce
+import top.yogiczy.mytv.core.data.entities.iptvsource.IptvSource.Companion.needExternalStoragePermission
 import top.yogiczy.mytv.tv.ui.material.Padding
 import top.yogiczy.mytv.tv.ui.material.PopupHandleableApplication
 import top.yogiczy.mytv.tv.ui.material.Snackbar
@@ -41,6 +53,10 @@ fun App(
     onBackPressed: () -> Unit = {},
     settingsViewModel: SettingsViewModel = viewModel(),
 ) {
+    if (settingsViewModel.iptvSourceCurrent.needExternalStoragePermission()) {
+        requestExternalStoragePermission()
+    }
+
     val configuration = LocalConfiguration.current
     val doubleBackPressedExitState = rememberDoubleBackPressedExitState()
 
@@ -128,3 +144,56 @@ fun rememberChildPadding(direction: LayoutDirection = LocalLayoutDirection.curre
             bottom = ParentPadding.calculateBottomPadding()
         )
     }
+
+@Composable
+fun requestExternalStoragePermission(): Boolean {
+    val context = LocalContext.current
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        fun isPermissionGranted(): Boolean {
+            return Environment.isExternalStorageManager()
+        }
+
+        var permissionGranted by remember { mutableStateOf(isPermissionGranted()) }
+
+        val intentLauncher = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.StartActivityForResult()
+        ) {
+            permissionGranted = isPermissionGranted()
+        }
+
+        LaunchedEffect(Unit) {
+            if (!permissionGranted) {
+                val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION).apply {
+                    data = Uri.parse("package:${context.packageName}")
+                }
+                intentLauncher.launch(intent)
+            }
+        }
+
+        return permissionGranted
+    } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        var permissionGranted by remember {
+            mutableStateOf(
+                ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            )
+        }
+
+        val launcher = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.RequestPermission()
+        ) { isGranted -> permissionGranted = isGranted }
+
+        LaunchedEffect(Unit) {
+            if (!permissionGranted) {
+                launcher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }
+
+        return permissionGranted
+    } else {
+        return true
+    }
+}
